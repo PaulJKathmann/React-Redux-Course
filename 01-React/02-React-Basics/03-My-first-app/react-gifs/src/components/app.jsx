5e1abd7612d1d51d05d2f7ad9864d2690c2f0242
PaulJKathmann <paul.kathmann.98@gmail.com>
2023-04-03 21:26:17 +0200

    added searchbar

--- 01-React/02-React-Basics/03-My-first-app/react-gifs/src/components/app.jsx
+++ 01-React/02-React-Basics/03-My-first-app/react-gifs/src/components/app.jsx
@@ -1,10 +1,12 @@
 import React, { Component} from 'react';
+import SearchBar from './searchbar';
 
 class App extends Component {
     render () {
         return(
             <div className="container">
                 <div className="left-scene">
+                    <SearchBar />
                 </div>
                 <div className="right-scene">
                 </div>
